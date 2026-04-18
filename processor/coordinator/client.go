4d8e1fc9ea7911e2cb9024c4bab50dbedbcb839a
pitr <pitr.vern@gmail.com>
2026-04-18 17:53:56 +0200

    feat: processor gRPC client with reconnect backoff

---
+++ processor/coordinator/client.go
@@ -0,0 +1,106 @@
+package coordinator
+
+import (
+	"context"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	gen "retroactivesampling/gen"
+)
+
+type DecisionHandler func(traceID string, keep bool)
+
+type Client struct {
+	endpoint string
+	handler  DecisionHandler
+	sendCh   chan string
+	ctx      context.Context
+	cancel   context.CancelFunc
+}
+
+func New(endpoint string, handler DecisionHandler) *Client {
+	ctx, cancel := context.WithCancel(context.Background())
+	c := &Client{
+		endpoint: endpoint,
+		handler:  handler,
+		sendCh:   make(chan string, 256),
+		ctx:      ctx,
+		cancel:   cancel,
+	}
+	go c.run()
+	return c
+}
+
+func (c *Client) Notify(traceID string) {
+	select {
+	case c.sendCh <- traceID:
+	default: // drop if backpressure
+	}
+}
+
+func (c *Client) Close() { c.cancel() }
+
+func (c *Client) run() {
+	backoff := time.Second
+	for {
+		if err := c.connect(); err != nil {
+			select {
+			case <-c.ctx.Done():
+				return
+			case <-time.After(backoff):
+				if backoff < 30*time.Second {
+					backoff *= 2
+				}
+			}
+		} else {
+			backoff = time.Second
+		}
+	}
+}
+
+func (c *Client) connect() error {
+	conn, err := grpc.NewClient(c.endpoint,
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	stream, err := gen.NewCoordinatorClient(conn).Connect(c.ctx)
+	if err != nil {
+		return err
+	}
+
+	recvErr := make(chan error, 1)
+	go func() {
+		for {
+			msg, err := stream.Recv()
+			if err != nil {
+				recvErr <- err
+				return
+			}
+			if d := msg.GetDecision(); d != nil {
+				c.handler(d.TraceId, d.Keep)
+			}
+		}
+	}()
+
+	for {
+		select {
+		case traceID := <-c.sendCh:
+			if err := stream.Send(&gen.ProcessorMessage{
+				Payload: &gen.ProcessorMessage_Notify{
+					Notify: &gen.NotifyInteresting{TraceId: traceID},
+				},
+			}); err != nil {
+				return err
+			}
+		case err := <-recvErr:
+			return err
+		case <-c.ctx.Done():
+			return nil
+		}
+	}
+}
---
+++ processor/coordinator/client_test.go
@@ -0,0 +1,106 @@
+package coordinator_test
+
+import (
+	"io"
+	"net"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+
+	gen "retroactivesampling/gen"
+	coord "retroactivesampling/processor/coordinator"
+)
+
+// fakeServer records notifications and allows broadcasting decisions.
+type fakeServer struct {
+	gen.UnimplementedCoordinatorServer
+	mu       sync.Mutex
+	notified []string
+	streams  []gen.Coordinator_ConnectServer
+}
+
+func (f *fakeServer) Connect(stream gen.Coordinator_ConnectServer) error {
+	f.mu.Lock()
+	f.streams = append(f.streams, stream)
+	f.mu.Unlock()
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if n := msg.GetNotify(); n != nil {
+			f.mu.Lock()
+			f.notified = append(f.notified, n.TraceId)
+			f.mu.Unlock()
+		}
+	}
+}
+
+func (f *fakeServer) broadcast(traceID string) {
+	msg := &gen.CoordinatorMessage{
+		Payload: &gen.CoordinatorMessage_Decision{
+			Decision: &gen.TraceDecision{TraceId: traceID, Keep: true},
+		},
+	}
+	f.mu.Lock()
+	for _, s := range f.streams {
+		_ = s.Send(msg)
+	}
+	f.mu.Unlock()
+}
+
+func startFakeServer(t *testing.T) (*fakeServer, string) {
+	t.Helper()
+	srv := &fakeServer{}
+	lis, err := net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(t, err)
+	gs := grpc.NewServer()
+	gen.RegisterCoordinatorServer(gs, srv)
+	go func() { _ = gs.Serve(lis) }()
+	t.Cleanup(gs.Stop)
+	return srv, lis.Addr().String()
+}
+
+func TestClientSendsNotification(t *testing.T) {
+	srv, addr := startFakeServer(t)
+	received := make(chan string, 1)
+	c := coord.New(addr, func(traceID string, keep bool) { received <- traceID })
+	t.Cleanup(c.Close)
+
+	time.Sleep(100 * time.Millisecond) // connection establishment
+	c.Notify("trace-99")
+	time.Sleep(100 * time.Millisecond)
+
+	srv.mu.Lock()
+	notified := srv.notified
+	srv.mu.Unlock()
+	require.Contains(t, notified, "trace-99")
+}
+
+func TestClientReceivesDecision(t *testing.T) {
+	srv, addr := startFakeServer(t)
+	received := make(chan string, 1)
+	c := coord.New(addr, func(traceID string, keep bool) {
+		if keep {
+			received <- traceID
+		}
+	})
+	t.Cleanup(c.Close)
+
+	time.Sleep(100 * time.Millisecond)
+	srv.broadcast("trace-55")
+
+	select {
+	case id := <-received:
+		assert.Equal(t, "trace-55", id)
+	case <-time.After(time.Second):
+		t.Fatal("timed out waiting for decision")
+	}
+}
