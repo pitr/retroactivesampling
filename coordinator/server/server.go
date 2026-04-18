64115301eb51af114ee0a846b17b696a29cea804
pitr <pitr.vern@gmail.com>
2026-04-18 17:38:09 +0200

    feat: coordinator gRPC server with processor registry

---
+++ coordinator/server/server.go
@@ -0,0 +1,71 @@
+package server
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"sync"
+
+	gen "retroactivesampling/gen"
+)
+
+type Server struct {
+	gen.UnimplementedCoordinatorServer
+	mu       sync.RWMutex
+	streams  map[string]gen.Coordinator_ConnectServer
+	onNotify func(traceID string)
+}
+
+func New(onNotify func(traceID string)) *Server {
+	return &Server{
+		streams:  make(map[string]gen.Coordinator_ConnectServer),
+		onNotify: onNotify,
+	}
+}
+
+func (s *Server) Connect(stream gen.Coordinator_ConnectServer) error {
+	id := randomID()
+	s.mu.Lock()
+	s.streams[id] = stream
+	s.mu.Unlock()
+	defer func() {
+		s.mu.Lock()
+		delete(s.streams, id)
+		s.mu.Unlock()
+	}()
+
+	for {
+		msg, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		if n := msg.GetNotify(); n != nil {
+			s.onNotify(n.TraceId)
+		}
+	}
+}
+
+// Broadcast sends keep decision to all connected processors. Best-effort.
+func (s *Server) Broadcast(traceID string, keep bool) {
+	msg := &gen.CoordinatorMessage{
+		Payload: &gen.CoordinatorMessage_Decision{
+			Decision: &gen.TraceDecision{TraceId: traceID, Keep: keep},
+		},
+	}
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	for _, stream := range s.streams {
+		_ = stream.Send(msg)
+	}
+}
+
+func randomID() string {
+	b := make([]byte, 8)
+	if _, err := rand.Read(b); err != nil {
+		panic(err) // crypto/rand failure is unrecoverable
+	}
+	return hex.EncodeToString(b)
+}
---
+++ coordinator/server/server_test.go
@@ -0,0 +1,65 @@
+package server_test
+
+import (
+	"context"
+	"net"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	gen "retroactivesampling/gen"
+	"retroactivesampling/coordinator/server"
+)
+
+func startServer(t *testing.T, s *server.Server) string {
+	t.Helper()
+	lis, err := net.Listen("tcp", "127.0.0.1:0")
+	require.NoError(t, err)
+	gs := grpc.NewServer()
+	gen.RegisterCoordinatorServer(gs, s)
+	go func() { _ = gs.Serve(lis) }()
+	t.Cleanup(gs.Stop)
+	return lis.Addr().String()
+}
+
+func TestBroadcastToConnectedProcessors(t *testing.T) {
+	notified := make(chan string, 1)
+	srv := server.New(func(traceID string) { notified <- traceID })
+	addr := startServer(t, srv)
+
+	conn, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	require.NoError(t, err)
+	t.Cleanup(func() { _ = conn.Close() })
+
+	stream, err := gen.NewCoordinatorClient(conn).Connect(context.Background())
+	require.NoError(t, err)
+
+	// Processor sends notification
+	err = stream.Send(&gen.ProcessorMessage{
+		Payload: &gen.ProcessorMessage_Notify{
+			Notify: &gen.NotifyInteresting{TraceId: "trace-1"},
+		},
+	})
+	require.NoError(t, err)
+
+	select {
+	case id := <-notified:
+		assert.Equal(t, "trace-1", id)
+	case <-time.After(time.Second):
+		t.Fatal("timed out waiting for notification")
+	}
+
+	// Coordinator broadcasts back
+	srv.Broadcast("trace-1", true)
+
+	msg, err := stream.Recv()
+	require.NoError(t, err)
+	d := msg.GetDecision()
+	require.NotNil(t, d)
+	assert.Equal(t, "trace-1", d.TraceId)
+	assert.True(t, d.Keep)
+}
